70c68af2972d705615e2d4adaf63a73038d3dce3
jehyunJung <toojey7@gmail.com>
2022-08-01 22:33:49 +0900

    feat: add update/search function in posts service

--- springawsserver/src/main/java/web_service/springawsserver/domain/service/PostsService.java
+++ springawsserver/src/main/java/web_service/springawsserver/domain/service/PostsService.java
@@ -3,6 +3,7 @@ package web_service.springawsserver.domain.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import web_service.springawsserver.domain.entity.Posts;
 import web_service.springawsserver.domain.repository.PostsRepository;
 import web_service.springawsserver.web.dto.PostsSaveRequestDto;
 
@@ -14,6 +15,21 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
          return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto updateRequestDto) {
+        Posts post = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다. id=" + id));
+        post.update(updateRequestDto.getTitle(), updateRequestDto.getContent());
+        return id;
+    }
+
+    @Transactional
+    public PostsResponseDto findById(Long id) {
+        Posts post = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다. id=" + id));
+        return new PostsResponseDto(post);
     }
 
 }
