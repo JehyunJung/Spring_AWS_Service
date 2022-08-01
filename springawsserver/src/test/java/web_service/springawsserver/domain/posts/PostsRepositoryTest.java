c2cc79c491d6d6a19ca138b3e4196ef976729461
jehyunJung <toojey7@gmail.com>
2022-08-01 21:45:09 +0900

    feat: add domain classes for Posts
    Posts Entity
    Posts Repository
    Posts Reposiotry Test

---
+++ springawsserver/src/test/java/web_service/springawsserver/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,44 @@
+package web_service.springawsserver.domain.posts;
+
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    void saveAndFind() {
+        //given
+        String title = "Test";
+        String content = "Test Content";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("testUser")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
