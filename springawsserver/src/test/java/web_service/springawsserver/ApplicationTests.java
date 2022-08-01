581b99cd97fe5cfd30a5bbda4e84f364033bf585
jehyunJung <toojey7@gmail.com>
2022-08-01 12:56:35 +0900

    init: initial Settings for Spring Application

---
+++ springawsserver/src/test/java/web_service/springawsserver/ApplicationTests.java
@@ -0,0 +1,13 @@
+package web_service.springawsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
