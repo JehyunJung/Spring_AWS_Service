8880b33ae94e6fc0817e461052a18280a5e380ca
jehyunJung <toojey7@gmail.com>
2022-08-01 18:46:48 +0900

    add: basic hello controller
    controller , response dto

---
+++ springawsserver/src/main/java/web_service/springawsserver/web/dto/HelloResponseDto.java
@@ -0,0 +1,11 @@
+package web_service.springawsserver.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
