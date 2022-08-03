c86516a75e39f43dd787fad61e1faedf417b9fec
jehyunJung <toojey7@gmail.com>
2022-08-03 22:43:21 +0900

    web: add web based functions for post update, delete
    Post Controller, Post Api Controller --> Post related controller
    IndexController --> Homepage controller

--- springawsserver/src/main/java/web_service/springawsserver/web/controller/IndexController.java
+++ springawsserver/src/main/java/web_service/springawsserver/web/controller/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import web_service.springawsserver.domain.entity.Posts;
 import web_service.springawsserver.domain.service.PostsService;
@@ -18,6 +19,7 @@ import java.util.List;
 @Controller
 @RequiredArgsConstructor
 @Slf4j
+
 public class IndexController {
     private final PostsService postsService;
 
@@ -25,27 +27,5 @@ public class IndexController {
     @GetMapping("/")
     public String index() {
         return "index";
-    }
-
-    //게시글을 등록하는 폼을 반환
-    @GetMapping("/posts/save")
-    public String saveForm(@ModelAttribute("posts") PostsSaveRequestDto posts, Model model) {
-        return "posts/save";
-    }
-
-    //게시글을 실제로 등록하는 작업
-    @PostMapping("/posts/save")
-    public String saveProcess(@ModelAttribute("posts") PostsSaveRequestDto posts, Model model) {
-        log.info("posts data -> title:{},author:{},content:{}", posts.getTitle(), posts.getAuthor(), posts.getContent());
-        postsService.save(posts);
-        return "redirect:/posts/allPosts";
-    }
-
-    //모든 게시글을 출력하는 작업
-    @GetMapping("/posts/allPosts")
-    public String allPosts(Model model){
-        List<PostsListResponseDto> posts = postsService.findAll();
-        model.addAttribute("posts", posts);
-        return "posts/allPosts";
     }
 }
