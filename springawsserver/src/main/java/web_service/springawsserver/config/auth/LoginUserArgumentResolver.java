1a1253bdbd0f233f58326617450fbd0ce6058045
jehyunJung <toojey7@gmail.com>
2022-08-05 22:10:32 +0900

    feat: add LoginMember annotation
    Customized Annotation
    ArgumentResolver

---
+++ springawsserver/src/main/java/web_service/springawsserver/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,30 @@
+package web_service.springawsserver.config.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import web_service.springawsserver.config.auth.dto.SessionMember;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+    private final HttpSession httpSession;
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginMember.class) != null;
+        boolean isMemberClass = parameter.getParameterType().equals(SessionMember.class);
+
+        return isLoginUserAnnotation && isMemberClass;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return httpSession.getAttribute("member");
+    }
+}
